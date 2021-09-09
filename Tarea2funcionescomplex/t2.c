a9c220b33b592d0aba613def1090c04390483514
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-09-08 19:25:23 -0500

    update
    codigo 2,4,6,5 ya funcionan

--- Tarea2funcionescomplex/t2.c
+++ Tarea2funcionescomplex/t2.c
@@ -44,22 +44,40 @@ void c3()
     int n_espacio = 0, n_tiempo = 0;
 
     int i, j, k, **a, **b, **c;
-    a = malloc(sizeof(int *) * (n * n));
-    b = malloc(sizeof(int *) * (n * n));
-    c = malloc(sizeof(int *) * (n * n));
+    n_espacio += 3;
+    n_espacio = n_espacio + (3 * n * n);
+    //Asignacion de espacio de matriz 2x2
+    a = malloc(sizeof(int *) * (n));
+    b = malloc(sizeof(int *) * (n));
+    c = malloc(sizeof(int *) * (n));
+
+    for (int m = 0; m < n; m++)
+    {
+        a[m] = malloc(sizeof(int));
+        b[m] = malloc(sizeof(int));
+        c[m] = malloc(sizeof(int));
+    }
 
     //*************************
-    /*for (i = 1; i < n; i++)
+    for (i = 1, n_tiempo++; i < n; n_tiempo++, i++)
     {
-        for (j = 1; j < n; j++)
+        n_tiempo++; //verdad
+        for (j = 1, n_tiempo++; j < n; n_tiempo++, j++)
         {
-            c[i, j] = 0;
-            for (k = 1; k < n; k++)
+            n_tiempo++; //verdad2
+            c[i][j] = 0;
+            n_tiempo++;
+            for (k = 1, n_tiempo++; k < n; n_tiempo++, k++)
             {
-                c[i, j] = c[i, j] + a[i, k] * b[k, j];
+                n_tiempo++; //verdad3
+                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                n_tiempo += 3;
             }
+            n_tiempo++; //falso3
         }
-    }*/
+        n_tiempo++; //falso2
+    }
+    n_tiempo++; //falso
     //*******************************
     printf("Espacio:%d\n", n_espacio);
     printf("Tiempo:%d\n", n_tiempo);
@@ -95,6 +113,69 @@ void c4()
     printf("Espacio:%d\n", n_espacio);
     printf("Tiempo:%d\n", n_tiempo);
 }
+void c5()
+{
+    int n = scanearn();
+    int n_espacio = 0, n_tiempo = 0;
+
+    //
+    int i, j, k, *s, *s2;
+    s = malloc(sizeof(int *) * (n));
+    s2 = malloc(sizeof(int *) * (n));
+    n_espacio += 3;
+    n_espacio = n_espacio + (2 * n);
+    //*******************************************
+    for (i = n - 1, j = 0, n_tiempo += 2; i >= 0; i--, j++, n_tiempo += 2)
+    {
+        n_tiempo++; //verdad1
+        s2[j] = s[i];
+        n_tiempo++;
+    }
+    n_tiempo++; //falso1
+    for (k = 0, n_tiempo++; k < n; n_tiempo++, k++)
+    {
+        n_tiempo++; //verdad2
+        s[i] = s2[i];
+        n_tiempo++;
+    }
+    n_tiempo++; //falso2
+    //***************************************************
+
+    printf("Espacio:%d\n", n_espacio);
+    printf("Tiempo:%d\n", n_tiempo);
+}
+void c6()
+{
+    printf("\nn no requerida.\n");
+    int n_espacio = 0, n_tiempo = 0;
+
+    //almacenamiento
+    int l, a, b, i, r;
+    n_espacio += 5;
+    //*********************************************
+    l = (a < b) ? a : b;
+    n_tiempo += 4; //operador terniario
+    r = 1;
+    n_tiempo++;
+    for (i = 2, n_tiempo++; i <= l; n_tiempo++, i++)
+    {
+        n_tiempo++; //verdad for
+        if (a % i == 0 && b % i == 0)
+        {
+            n_tiempo += 2; //verdadif
+            r = i;
+            n_tiempo++;
+        }
+        n_tiempo++; //falsoif
+    }
+    n_tiempo++; //falso for
+    printf("Espacio sin n:%d\n", n_espacio);
+    printf("Tiempo sin n:%d\n", n_tiempo);
+}
+void c7()
+{
+    printf("hola");
+}
 void menu()
 {
     int opc;
@@ -127,13 +208,13 @@ void menu()
             c4();
             break;
         case 5:
-
+            c5();
             break;
         case 6:
-
+            c6();
             break;
         case 7:
-
+            c7();
             break;
         case 8:
             exit(0);
