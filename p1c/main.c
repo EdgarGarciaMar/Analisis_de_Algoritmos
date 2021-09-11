c068fe112de327968431e929832a7d539426d23b
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2021-09-11 11:11:17 -0700

    Algoritmos de Ordenamiento P1
    Se agregaron el main, el tiempo_ord y tiempo_ord.h los cuales permiten ordenar numeros de un documento txt mediante redireccionamiento, tambien esta el script para ejecutar todo el programa. el archivo numeros10millones no se sube.

---
+++ p1c/main.c
@@ -0,0 +1,138 @@
+//*****************************************************************
+//M. EN C. EDGARDO ADRIÁN FRANCO MARTÍNEZ
+//Curso: Análisis de algoritmos
+//(C) Enero 2013
+//ESCOM-IPN
+//Ejemplo de medición de tiempo en C y recepción de parametros en C bajo UNIX
+//Compilación: "gcc main.c tiempo.x  -o main(teimpo.c si se tiene la implementación de la libreria o tiempo.o si solo se tiene el codigo objeto)"
+//Ejecución: "./main n" (Linux y MAC OS)
+//*****************************************************************
+
+//*****************************************************************
+//LIBRERIAS INCLUIDAS
+//*****************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tiempo_ord.h"
+//*****************************************************************
+//DEFINICION DE CONSTANTES DEL PROGRAMA
+//*****************************************************************
+
+//********************************************************************************
+//DECLARACION DE ESTRUCTURAS
+//********************************************************************************
+
+//*****************************************************************
+//DECLARACIÓN DE FUNCIONES
+//*****************************************************************
+
+//*****************************************************************
+//VARIABLES GLOBALES
+//*****************************************************************
+
+//*****************************************************************
+//PROGRAMA PRINCIPAL
+//*****************************************************************
+int main(int argc, char *argv[])
+{
+	//******************************************************************
+	//Variables del main
+	//******************************************************************
+	double utime0, stime0, wtime0, utime1, stime1, wtime1; //Variables para medición de tiempos
+	int n, opc;											   //n determina el tamaño del algorito dado por argumento al ejecutar
+	int i;												   //Variables para loops
+
+	//******************************************************************
+	//Recepción y decodificación de argumentos
+	//******************************************************************
+
+	//Si no se introducen exactamente 2 argumentos (Cadena de ejecución y cadena=n)
+	if (argc != 3)
+	{
+		printf("\nIndique el tamanio del algoritmo - Ejemplo: [user@equipo]$ %s 100\n", argv[0]);
+		exit(1);
+	}
+	//Tomar el segundo argumento como tamaño del algoritmo
+	else
+	{
+		n = atoi(argv[1]);
+		opc = atoi(argv[2]);
+	}
+	// Lectura del archivo numeros10millones.txt mediante redireccionamiento.
+	int A[n];
+	for (int a = 0; a < n; a++)
+	{
+		scanf("%d", &A[a]);
+		//printf("[%d]=%d\n", a, A[a]);
+	}
+	//******************************************************************
+	//Iniciar el conteo del tiempo para las evaluaciones de rendimiento
+	//******************************************************************
+	switch (opc)
+	{
+	case 1:
+		uswtime(&utime0, &stime0, &wtime0);
+		BurbujaSimple(A, n);
+		uswtime(&utime1, &stime1, &wtime1);
+		break;
+	case 2:
+		uswtime(&utime0, &stime0, &wtime0);
+		Burbujaop1(A, n);
+		uswtime(&utime1, &stime1, &wtime1);
+		break;
+	case 3:
+		uswtime(&utime0, &stime0, &wtime0);
+		Burbujaop2(A, n);
+		uswtime(&utime1, &stime1, &wtime1);
+		break;
+	default:
+		printf("\n Error opc.\n");
+		exit(1);
+		break;
+	}
+	//uswtime(&utime0, &stime0, &wtime0);
+	//******************************************************************
+
+	//******************************************************************
+	//Algoritmo
+	//******************************************************************
+	//for (i = 0; i < n; i++)
+	//	for (i = 0; i < n; i++)
+	//		n = n;
+	//******************************************************************
+
+	//******************************************************************
+	//Evaluar los tiempos de ejecución
+	//******************************************************************
+	// uswtime(&utime1, &stime1, &wtime1);
+
+	//Cálculo del tiempo de ejecución del programa
+	printf("\n");
+	printf("real (Tiempo total)  %.10f s\n", wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10f s\n", utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10f s\n", stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n", 100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+
+	//Mostrar los tiempos en formato exponecial
+	printf("\n");
+	printf("real (Tiempo total)  %.10e s\n", wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10e s\n", utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10e s\n", stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n", 100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+	//******************************************************************
+	//*************Imprimir el arreglo ordenado
+	printf("\n opc = %d , Los numeros ordenados son:\n", opc);
+	for (int a = 0; a < n; a++)
+	{
+		printf("[%d]=%d\n", a, A[a]);
+	}
+	//Terminar programa normalmente
+	exit(0);
+}
+
+//************************************************************************
+//DEFINICIÓN DE FUNCIONES
+//************************************************************************
