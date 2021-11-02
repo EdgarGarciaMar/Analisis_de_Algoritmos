42e39cf32364f2265526c559442711bc866f1b07
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-11-02 15:40:34 -0600

    update

--- p3copia/compres.c
+++ p3copia/compres.c
@@ -12,13 +12,13 @@
 /* se usan los punteros cero y uno */
 typedef struct _nodo
 {
-   unsigned char letra;    /* Letra a la que hace referencia el nodo */
-   int frecuencia;         /* veces que aparece la letra en el texto o las letras */
-                           /* de los nodos de las ramas cero y uno */
-   struct _nodo *sig;      /* Puntero a siguiente nodo de una lista enlazada */
-   struct _nodo *cero;     /* Puntero a la rama cero de un árbol */
-   struct _nodo *uno;      /* Puntero a la rama uno de un árbol */
-} tipoNodo;                /* Nombre de tipo */
+   unsigned char letra; /* Letra a la que hace referencia el nodo */
+   int frecuencia;      /* veces que aparece la letra en el texto o las letras */
+                        /* de los nodos de las ramas cero y uno */
+   struct _nodo *sig;   /* Puntero a siguiente nodo de una lista enlazada */
+   struct _nodo *cero;  /* Puntero a la rama cero de un árbol */
+   struct _nodo *uno;   /* Puntero a la rama uno de un árbol */
+} tipoNodo;             /* Nombre de tipo */
 
 /* Nodo para construir una lista para la tabla de codigos */
 typedef struct _tabla
@@ -33,9 +33,9 @@ typedef struct _tabla
 tipoTabla *Tabla;
 
 /* Prototipos */
-void Cuenta(tipoNodo** Lista, unsigned char c);
-void Ordenar(tipoNodo** Lista);
-void InsertarOrden(tipoNodo** Cabeza, tipoNodo *e);
+void Cuenta(tipoNodo **Lista, unsigned char c);
+void Ordenar(tipoNodo **Lista);
+void InsertarOrden(tipoNodo **Cabeza, tipoNodo *e);
 void BorrarArbol(tipoNodo *n);
 void CrearTabla(tipoNodo *n, int l, int v);
 void InsertarTabla(unsigned char c, int l, int v);
@@ -43,11 +43,11 @@ tipoTabla *BuscaCaracter(tipoTabla *Tabla, unsigned char c);
 
 int main(int argc, char *argv[])
 {
-   tipoNodo *Lista;       /* Lista de letras y frecuencias */
-   tipoNodo *Arbol;       /* Arbol de letras y frecuencias */
+   tipoNodo *Lista; /* Lista de letras y frecuencias */
+   tipoNodo *Arbol; /* Arbol de letras y frecuencias */
 
-   FILE *fe, *fs;         /* Ficheros de entrada y salida */
-   unsigned char c;       /* variables auxiliares */
+   FILE *fe, *fs;   /* Ficheros de entrada y salida */
+   unsigned char c; /* variables auxiliares */
    tipoNodo *p;
    tipoTabla *t;
    int nElementos;        /* Número de elementos en tabla */
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
    unsigned long int dWORD; /* Soble palabra usada durante la codificación */
    int nBits;               /* Número de bits usados de dWORD */
 
-   if(argc < 3)
+   if (argc < 3)
    {
       printf("Usar:\n%s <fichero_entrada> <fichero_salida>\n", argv[0]);
       return 1;
@@ -65,22 +65,47 @@ int main(int argc, char *argv[])
    Lista = NULL;
    /* Fase 1: contar frecuencias */
    fe = fopen(argv[1], "r");
-   do {
+   do
+   {
       c = fgetc(fe);
-      if( feof(fe) ) {
-         break ;
+      if (feof(fe))
+      {
+         break;
       }
       Longitud++;        /* Actualiza la cuenta de la longitud del fichero */
       Cuenta(&Lista, c); /* Actualiza la lista de frecuencias */
-   } while(1);
+   } while (1);
    fclose(fe);
 
    /* Ordenar la lista de menor a mayor */
    Ordenar(&Lista);
 
+   tipoNodo *aux;
+   aux = Lista;
+
+   FILE *fs2, *fs3;
+
+   fs2 = fopen("frecuencias.txt", "w");
+   while (aux != NULL)
+   {
+      fwrite(&aux->letra, sizeof(char), 1, fs2);
+      fwrite(&aux->frecuencia, sizeof(long int), 1, fs2);
+      aux = aux->sig;
+   }
+   fclose(fs2);
+
+   //fs3 = fopen("frecuencias.txt", "r");
+   //while (!feof(fs3))
+   //{
+   // fread(&hola, sizeof(char), 1, fs3);
+   //fread(&aux->letra, sizeof(char), 1, fs3);
+   //fread(&aux->frecuencia, sizeof(long int), 1, fs3);
+   //}
+   //fclose(fs3);
+
    /* Crear el arbol */
    Arbol = Lista;
-   while(Arbol && Arbol->sig) /* Mientras existan al menos dos elementos en la lista */
+   while (Arbol && Arbol->sig) /* Mientras existan al menos dos elementos en la lista */
    {
       p = (tipoNodo *)malloc(sizeof(tipoNodo)); /* Un nuevo árbol */
       p->letra = 0;                             /* No corresponde a ninguna letra */
@@ -89,9 +114,9 @@ int main(int argc, char *argv[])
       p->cero = Arbol;                          /* Rama cero */
       Arbol = Arbol->sig;                       /* Siguiente nodo */
       p->frecuencia = p->uno->frecuencia +
-                      p->cero->frecuencia;      /* Suma de frecuencias */
-      InsertarOrden(&Arbol, p);                 /* Inserta en nuevo nodo */
-   }                                            /* orden de frecuencia */
+                      p->cero->frecuencia; /* Suma de frecuencias */
+      InsertarOrden(&Arbol, p);            /* Inserta en nuevo nodo */
+   }                                       /* orden de frecuencia */
 
    /* Construir la tabla de códigos binarios */
    Tabla = NULL;
@@ -104,7 +129,7 @@ int main(int argc, char *argv[])
    /* Cuenta el número de elementos de tabla */
    nElementos = 0;
    t = Tabla;
-   while(t)
+   while (t)
    {
       nElementos++;
       t = t->sig;
@@ -113,7 +138,7 @@ int main(int argc, char *argv[])
    fwrite(&nElementos, sizeof(int), 1, fs);
    /* Escribir tabla en fichero */
    t = Tabla;
-   while(t)
+   while (t)
    {
       fwrite(&t->letra, sizeof(char), 1, fs);
       fwrite(&t->bits, sizeof(unsigned long int), 1, fs);
@@ -125,41 +150,45 @@ int main(int argc, char *argv[])
    fe = fopen(argv[1], "r");
    dWORD = 0; /* Valor inicial. */
    nBits = 0; /* Ningún bit */
-   do {
+   do
+   {
       c = fgetc(fe);
-      if( feof(fe) ) {
-         break ;
+      if (feof(fe))
+      {
+         break;
       }
       /* Busca c en tabla: */
       t = BuscaCaracter(Tabla, c);
       /* Si nBits + t->nbits > 32, sacar un byte */
-      while(nBits + t->nbits > 32)
+      while (nBits + t->nbits > 32)
       {
-         c = dWORD >> (nBits-8);           /* Extrae los 8 bits de mayor peso */
-         fwrite(&c, sizeof(char), 1, fs);  /* Y lo escribe en el fichero */
-         nBits -= 8;                       /* Ya tenemos hueco para 8 bits más */
+         c = dWORD >> (nBits - 8);        /* Extrae los 8 bits de mayor peso */
+         fwrite(&c, sizeof(char), 1, fs); /* Y lo escribe en el fichero */
+         nBits -= 8;                      /* Ya tenemos hueco para 8 bits más */
       }
       dWORD <<= t->nbits; /* Hacemos sitio para el nuevo caracter */
       dWORD |= t->bits;   /* Insertamos el nuevo caracter */
       nBits += t->nbits;  /* Actualizamos la cuenta de bits */
-   } while(1);
+   } while (1);
    /* Extraer los cuatro bytes que quedan en dWORD*/
-   while(nBits>0)
+   while (nBits > 0)
    {
-      if(nBits>=8) c = dWORD >> (nBits-8);
-      else c = dWORD << (8-nBits);
+      if (nBits >= 8)
+         c = dWORD >> (nBits - 8);
+      else
+         c = dWORD << (8 - nBits);
       fwrite(&c, sizeof(char), 1, fs);
       nBits -= 8;
    }
 
-   fclose(fe);  /* Cierra los ficheros */
+   fclose(fe); /* Cierra los ficheros */
    fclose(fs);
 
    /* Borrar Arbol */
    BorrarArbol(Arbol);
 
    /* Borrar Tabla */
-   while(Tabla)
+   while (Tabla)
    {
       t = Tabla;
       Tabla = t->sig;
@@ -173,15 +202,15 @@ int main(int argc, char *argv[])
 /* El puntero a Lista se pasa por referencia, ya que debe poder cambiar */
 /* Ya sea por que la lista esté vacía, o porque el nuevo elemento sea el */
 /* primero */
-void Cuenta(tipoNodo** Lista, unsigned char c)
+void Cuenta(tipoNodo **Lista, unsigned char c)
 {
    tipoNodo *p, *a, *q;
 
-   if(!*Lista)  /* Si la lista está vacía, el nuevo nodo será Lista */
+   if (!*Lista) /* Si la lista está vacía, el nuevo nodo será Lista */
    {
-      *Lista = (tipoNodo *)malloc(sizeof(tipoNodo));   /* Un nodo nuevo */
-      (*Lista)->letra = c;                             /* Para c */
-      (*Lista)->frecuencia = 1;                        /* en su 1ª aparición */
+      *Lista = (tipoNodo *)malloc(sizeof(tipoNodo)); /* Un nodo nuevo */
+      (*Lista)->letra = c;                           /* Para c */
+      (*Lista)->frecuencia = 1;                      /* en su 1ª aparición */
       (*Lista)->sig = (*Lista)->cero = (*Lista)->uno = NULL;
    }
    else
@@ -189,14 +218,15 @@ void Cuenta(tipoNodo** Lista, unsigned char c)
       /* Buscar el caracter en la lista (ordenada por letra) */
       p = *Lista;
       a = NULL;
-      while(p && p->letra < c)
+      while (p && p->letra < c)
       {
          a = p;      /* Guardamos el elemento actual para insertar */
          p = p->sig; /* Avanzamos al siguiente */
       }
       /* Dos casos: */
       /* 1) La letra es c se encontró */
-      if(p && p->letra == c) p->frecuencia++; /* Actualizar frecuencia */
+      if (p && p->letra == c)
+         p->frecuencia++; /* Actualizar frecuencia */
       else
       /* 2) La letra c no se encontró*/
       {
@@ -205,25 +235,28 @@ void Cuenta(tipoNodo** Lista, unsigned char c)
          q->letra = c;
          q->frecuencia = 1;
          q->cero = q->uno = NULL;
-         q->sig = p;        /* Insertar entre los nodos p */
-         if(a) a->sig = q;  /* y a */
-         else *Lista = q;    /* Si a es NULL el nuevo es el primero */
+         q->sig = p; /* Insertar entre los nodos p */
+         if (a)
+            a->sig = q; /* y a */
+         else
+            *Lista = q; /* Si a es NULL el nuevo es el primero */
       }
    }
 }
 
 /* Ordena Lista de menor a mayor por frecuencias */
 /* De nuevo pasamos el puntero a lista por referencia */
-void Ordenar(tipoNodo** Lista)
+void Ordenar(tipoNodo **Lista)
 {
    tipoNodo *Lista2, *a;
 
-   if(!*Lista) return; /* Lista vacia */
+   if (!*Lista)
+      return; /* Lista vacia */
    Lista2 = *Lista;
    *Lista = NULL;
-   while(Lista2)
+   while (Lista2)
    {
-      a = Lista2;              /* Toma los elementos de Lista2 */
+      a = Lista2; /* Toma los elementos de Lista2 */
       Lista2 = a->sig;
       InsertarOrden(Lista, a); /* Y los inserta por orden en Lista */
    }
@@ -231,39 +264,44 @@ void Ordenar(tipoNodo** Lista)
 
 /* Inserta el elemento e en la Lista ordenado por frecuencia de menor a mayor */
 /* El puntero a Cabeza se pasa por referencia */
-void InsertarOrden(tipoNodo** Cabeza, tipoNodo *e)
+void InsertarOrden(tipoNodo **Cabeza, tipoNodo *e)
 {
    tipoNodo *p, *a;
 
-   if(!*Cabeza) /* Si Cabeza es NULL, es el primer elemento */
+   if (!*Cabeza) /* Si Cabeza es NULL, es el primer elemento */
    {
       *Cabeza = e;
       (*Cabeza)->sig = NULL;
    }
    else
    {
-       /* Buscar el caracter en la lista (ordenada por frecuencia) */
-       p = *Cabeza;
-       a = NULL;
-       while(p && p->frecuencia < e->frecuencia)
-       {
-          a = p;      /* Guardamos el elemento actual para insertar */
-          p = p->sig; /* Avanzamos al siguiente */
-       }
-       /* Insertar el elemento */
-       e->sig = p;
-       if(a) a->sig = e;   /* Insertar entre a y p */
-       else *Cabeza = e;    /* el nuevo es el primero */
-    }
+      /* Buscar el caracter en la lista (ordenada por frecuencia) */
+      p = *Cabeza;
+      a = NULL;
+      while (p && p->frecuencia < e->frecuencia)
+      {
+         a = p;      /* Guardamos el elemento actual para insertar */
+         p = p->sig; /* Avanzamos al siguiente */
+      }
+      /* Insertar el elemento */
+      e->sig = p;
+      if (a)
+         a->sig = e; /* Insertar entre a y p */
+      else
+         *Cabeza = e; /* el nuevo es el primero */
+   }
 }
 
 /* Función recursiva para crear Tabla */
 /* Recorre el árbol cuya raiz es n y le asigna el código v de l bits */
 void CrearTabla(tipoNodo *n, int l, int v)
 {
-   if(n->uno)  CrearTabla(n->uno, l+1, (v<<1)|1);
-   if(n->cero) CrearTabla(n->cero, l+1, v<<1);
-   if(!n->uno && !n->cero) InsertarTabla(n->letra, l, v);
+   if (n->uno)
+      CrearTabla(n->uno, l + 1, (v << 1) | 1);
+   if (n->cero)
+      CrearTabla(n->cero, l + 1, v << 1);
+   if (!n->uno && !n->cero)
+      InsertarTabla(n->letra, l, v);
 }
 
 /* Insertar un elemento en la tabla */
@@ -276,26 +314,28 @@ void InsertarTabla(unsigned char c, int l, int v)
    t->bits = v;
    t->nbits = l;
 
-   if(!Tabla)         /* Si tabla es NULL, entonces el elemento t es el 1º */
+   if (!Tabla) /* Si tabla es NULL, entonces el elemento t es el 1º */
    {
       Tabla = t;
       Tabla->sig = NULL;
    }
    else
    {
-       /* Buscar el caracter en la lista (ordenada por frecuencia) */
-       p = Tabla;
-       a = NULL;
-       while(p && p->letra < t->letra)
-       {
-          a = p;      /* Guardamos el elemento actual para insertar */
-          p = p->sig; /* Avanzamos al siguiente */
-       }
-       /* Insertar el elemento */
-       t->sig = p;
-       if(a) a->sig = t;  /* Insertar entre a y p */
-       else Tabla = t;    /* el nuevo es el primero */
-    }
+      /* Buscar el caracter en la lista (ordenada por frecuencia) */
+      p = Tabla;
+      a = NULL;
+      while (p && p->letra < t->letra)
+      {
+         a = p;      /* Guardamos el elemento actual para insertar */
+         p = p->sig; /* Avanzamos al siguiente */
+      }
+      /* Insertar el elemento */
+      t->sig = p;
+      if (a)
+         a->sig = t; /* Insertar entre a y p */
+      else
+         Tabla = t; /* el nuevo es el primero */
+   }
 }
 
 /* Buscar un caracter en la tabla, devielve un puntero al elemento de la tabla */
@@ -304,14 +344,40 @@ tipoTabla *BuscaCaracter(tipoTabla *Tabla, unsigned char c)
    tipoTabla *t;
 
    t = Tabla;
-   while(t && t->letra != c) t = t->sig;
+   while (t && t->letra != c)
+      t = t->sig;
    return t;
 }
 
 /* Función recursiva para borrar un arbol */
 void BorrarArbol(tipoNodo *n)
 {
-   if(n->cero) BorrarArbol(n->cero);
-   if(n->uno)  BorrarArbol(n->uno);
+   if (n->cero)
+      BorrarArbol(n->cero);
+   if (n->uno)
+      BorrarArbol(n->uno);
    free(n);
 }
+/*
+void tablaFrecuencia(tipoTabla *tab, char *argv[])
+{
+
+   
+   unsigned char letra;    Letra a la que hace referencia el nodo 
+   unsigned long int bits; Valor de la codificación de la letra 
+   unsigned char nbits;    Número de bits de la codificación 
+   
+   tipoTabla *aux;
+   aux = Tabla;
+
+   FILE *fs2;
+
+   fs2 = fopen(argv[3], "w");
+   while (aux != NULL)
+   {
+      fwrite(&aux->letra, sizeof(char), 1, fs2);
+      fwrite(&aux->bits, sizeof(long int), 1, fs2);
+      aux = aux->sig;
+   }
+   fclose(fs2);
+}*/
