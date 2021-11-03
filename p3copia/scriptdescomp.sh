16d832bd04a8be5c4811f39f2b6d3ffcec2e6ca5
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-11-02 19:30:50 -0700

    Update Final
    Se agregaron los archivos de prueba

--- p3copia/scriptdescomp.sh
+++ p3copia/scriptdescomp.sh
@@ -1,2 +1,11 @@
 gcc decompres.c -o decompres
-time(./decompres codificado.dat entrada.txt) > porcentaje_de_comprecion.txt
+#ejecutar uno a la vez, porque el programa se hace bolas
+#time(./decompres codificado.dat entrada.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada2.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada3.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada4.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada5.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada6.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada7.txt) > porcentaje_de_comprecion.txt
+#time(./decompres codificado.dat entrada8.txt) > porcentaje_de_comprecion.txt
+time(./decompres codificado.dat entrada9.txt) > porcentaje_de_comprecion.txt
