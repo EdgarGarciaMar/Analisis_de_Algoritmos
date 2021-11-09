71bc38d9d7d8dd24431202c78da457a314203942
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-11-09 13:27:29 -0600

    Cota o

--- p1DV/DV1.cpp
+++ p1DV/DV1.cpp
@@ -4,69 +4,69 @@ using namespace std;
 
 long int i;
 
-long int max(long int num1, long int num2, long int num3)
+long int max(long int num1, long int num2, long int num3) //O(1)
 {
-    if (num1 >= num2 && num1 >= num3)
-        return num1;
+    if (num1 >= num2 && num1 >= num3) //O(1)
+        return num1;                  //O(1)
     else if (num2 > num3)
-        return num2;
+        return num2; //O(1)
     else
-        return num3;
+        return num3; //O(1)
 }
 
-long int sumaMaxima(long int A[], long int inicio, long int fin)
+long int sumaMaxima(long int A[], long int inicio, long int fin) //O(nlog(n))
 {
     long int mitad, izq, der, suma, sumaIzquierda, sumaDerecha, sumaCentral;
 
-    if (inicio == fin)
+    if (inicio == fin) //O(1)
     {
-        return A[inicio];
+        return A[inicio]; //O(1)
     }
-    mitad = (inicio + fin) / 2;
+    mitad = (inicio + fin) / 2; //O(1)
 
-    izq = sumaMaxima(A, inicio, mitad);
-    der = sumaMaxima(A, mitad + 1, fin);
-    suma = 0;
-    sumaIzquierda = A[mitad];
+    izq = sumaMaxima(A, inicio, mitad);  //O(nlog(n)) debido a la recursion 2T(n/2)+n
+    der = sumaMaxima(A, mitad + 1, fin); //O(nlog(n))
+    suma = 0;                            //O(1)
+    sumaIzquierda = A[mitad];            //O(1)
     for (i = mitad; i >= inicio; i--)
     {
-        suma += A[i];
-        if (suma > sumaIzquierda)
-            sumaIzquierda = suma;
+        suma += A[i];             //O(1)
+        if (suma > sumaIzquierda) //O(1)
+            sumaIzquierda = suma; //O(1)
     }
-    suma = 0;
-    sumaDerecha = A[mitad + 1];
+    suma = 0;                   //O(1)
+    sumaDerecha = A[mitad + 1]; //O(1)
     for (i = mitad + 1; i <= fin; i++)
     {
-        suma += A[i];
-        if (suma > sumaDerecha)
-            sumaDerecha = suma;
+        suma += A[i];           //O(1)
+        if (suma > sumaDerecha) //O(1)
+            sumaDerecha = suma; //O(1)
     }
-    sumaCentral = sumaIzquierda + sumaDerecha;
+    sumaCentral = sumaIzquierda + sumaDerecha; //O(1)
 
-    return max(izq, der, sumaCentral);
+    return max(izq, der, sumaCentral); //O(1)
 }
 
-int main()
+int main() //O(n)
 {
     std::cin.tie(nullptr);
     std::ios_base::sync_with_stdio(false);
 
     // TODO: fixme.
     long int n;
-    std::cin >> n;
-    long int A[n];
-    for (i = 0; i < n; i++)
+    std::cin >> n;          //O(1)
+    long int A[n];          //O(1)
+    for (i = 0; i < n; i++) //O(n)
     {
-        std::cin >> A[i];
+        std::cin >> A[i]; //O(1)
     }
-    if (n == 1)
+    if (n == 1) //O(1)
     {
-        std::cout << n + A[0];
+        std::cout << n + A[0]; //O(1)
     }
     else
     {
-        std::cout << sumaMaxima(A, 0, n);
+        std::cout << sumaMaxima(A, 0, n); //O(1)
     }
     return 0;
 }
