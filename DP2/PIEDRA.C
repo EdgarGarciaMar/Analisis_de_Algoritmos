a9d1a2daea6abdd99719182d7bca885ff0fe6e63
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-11-27 17:36:15 -0600

    Cota O

--- DP2/PIEDRA.C
+++ DP2/PIEDRA.C
@@ -1,31 +1,41 @@
 #include <stdio.h>
-int max(int a,int b)
+int max(int a, int b) //O(1)
 {
-  return (a>b)?a:b;
+  return (a > b) ? a : b; //O(1)
 }
 int main()
 {
-  int t;
-  scanf("%d\n", &t);
-  while(t--)
+  int t;             //O(1)
+  scanf("%d\n", &t); //O(1)
+  while (t--)        //O(t*(h*w))
   {
-    int h,w,i,j;
-    scanf("%d%d", &h,&w);
-    int a[h][w],dp[h][w];
-    for(i=0;i<h;i++)for(j=0;j<w;j++){scanf("%d",&a[i][j] );dp[i][j]=0;}
-    for(i=0;i<w;i++)dp[0][i]=a[0][i];
-    for(i=1;i<h;i++)
-    {
-      for(j=0;j<w;j++)
+    int h, w, i, j;        //O(1)
+    scanf("%d%d", &h, &w); //O(1)
+    int a[h][w], dp[h][w]; //O(1)
+
+    for (i = 0; i < h; i++) //O(h*w)
+      for (j = 0; j < w; j++)
       {
-        int x=(j>0)?dp[i-1][j-1]:0;
-        int y=(j<w-1)?dp[i-1][j+1]:0;
-        int z=dp[i-1][j];
-        dp[i][j]=a[i][j]+max(x,max(y,z));
+        scanf("%d", &a[i][j]); //O(1)
+        dp[i][j] = 0;          //O(1)
+      }
+
+    for (i = 0; i < w; i++) //O(w)
+      dp[0][i] = a[0][i];   //O(1)
+
+    for (i = 1; i < h; i++) //O(h*w)
+    {
+      for (j = 0; j < w; j++)
+      {
+        int x = (j > 0) ? dp[i - 1][j - 1] : 0;     //O(1)
+        int y = (j < w - 1) ? dp[i - 1][j + 1] : 0; //O(1)
+        int z = dp[i - 1][j];                       //O(1)
+        dp[i][j] = a[i][j] + max(x, max(y, z));     //O(1)
       }
     }
-    int x=0;
-    for(i=0;i<w;i++)x=max(x,dp[h-1][i]);
-    printf("%d\n", x);
+    int x = 0;                  //O(1)
+    for (i = 0; i < w; i++)     //O(w)
+      x = max(x, dp[h - 1][i]); //O(1)
+    printf("%d\n", x);          //O(1)
   }
 }
