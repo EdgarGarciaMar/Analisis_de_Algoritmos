4739d04e1c851fa58610a0e673609beb692a373a
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-12-09 09:45:42 -0600

    update parte 1.1
    se cambiaron tiempos para que se viera mejor

--- Proyecto/js/app.js
+++ Proyecto/js/app.js
@@ -151,34 +151,34 @@ async function carAssembleTime(a , t , e , x) {
   
   if(first<second){
     bfs.path[0].addClass('highlighted');
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[1].addClass('highlighted'), 1000); 
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[2].addClass('highlighted'), 1000);
-    await sleep(2000);
+    await sleep(1000);
   }
   else{
     bfs2.path[0].addClass('highlighted');
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[1].addClass('highlighted'), 1000);
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[2].addClass('highlighted'), 1000);
-    await sleep(2000);
+    await sleep(1000);
   }
   if(first==second){
     bfs.path[0].addClass('highlighted');
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[1].addClass('highlighted'), 1000); 
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[2].addClass('highlighted'), 1000);
-    await sleep(2000);
+    await sleep(1000);
 
     bfs2.path[0].addClass('highlighted');
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[1].addClass('highlighted'), 1000);
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[2].addClass('highlighted'), 1000);
-    await sleep(2000);
+    await sleep(1000);
   }
   var iteracionsiguienteUP=new Array();
   var iteracioncambiolinUP=new Array();
@@ -202,27 +202,27 @@ async function carAssembleTime(a , t , e , x) {
       if(up<down){
         if(up==siguienteUP){
           setTimeout(bfs.path[3].addClass('highlighted'), 1000);//bc
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs.path[4].addClass('highlighted'), 1000);//c
-          await sleep(2000);
+          await sleep(1000);
         }else{
           setTimeout(bfs2.path[5].addClass('highlighted'), 1000);//hc
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs2.path[6].addClass('highlighted'), 1000);//c
-          await sleep(2000);
+          await sleep(1000);
         }
       }
       else{
         if(down==siguienteDOWN){
           setTimeout(bfs2.path[3].addClass('highlighted'), 1000);//hi
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs2.path[4].addClass('highlighted'), 1000);//i
-          await sleep(2000);
+          await sleep(1000);
         }else{
           setTimeout(bfs.path[5].addClass('highlighted'), 1000);//bi
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs.path[6].addClass('highlighted'), 1000);//i
-          await sleep(2000);
+          await sleep(1000);
         }
       }
       console.log("iteracion 1");
@@ -237,27 +237,27 @@ async function carAssembleTime(a , t , e , x) {
       if(up<down){
         if(up==siguienteUP){
           setTimeout(bfs.path[7].addClass('highlighted'), 1000);//cd
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs.path[8].addClass('highlighted'), 1000);//d
-          await sleep(2000);
+          await sleep(1000);
         }else{
           setTimeout(bfs2.path[9].addClass('highlighted'), 1000);//id
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs2.path[10].addClass('highlighted'), 1000);//d
-          await sleep(2000);
+          await sleep(1000);
         }
       }
       else{
         if(down==siguienteDOWN){
           setTimeout(bfs2.path[7].addClass('highlighted'), 1000);//ij
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs2.path[8].addClass('highlighted'), 1000);//j
-          await sleep(2000);
+          await sleep(1000);
         }else{
           setTimeout(bfs.path[9].addClass('highlighted'), 1000);//cj
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs.path[10].addClass('highlighted'), 1000);//j
-          await sleep(2000);
+          await sleep(1000);
         }
       }
       console.log("iteracion 2");
@@ -273,27 +273,27 @@ async function carAssembleTime(a , t , e , x) {
       if(up<down){
         if(up==siguienteUP){
           setTimeout(bfs.path[11].addClass('highlighted'), 1000);//de
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs.path[12].addClass('highlighted'), 1000);//e
-          await sleep(2000);
+          await sleep(1000);
         }else{
           setTimeout(bfs2.path[13].addClass('highlighted'), 1000);//je
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs2.path[14].addClass('highlighted'), 1000);//e
-          await sleep(2000);
+          await sleep(1000);
         }
       }
       else{
         if(down==siguienteDOWN){
           setTimeout(bfs2.path[11].addClass('highlighted'), 1000);//jk
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs2.path[12].addClass('highlighted'), 1000);//k
-          await sleep(2000);
+          await sleep(1000);
         }else{
           setTimeout(bfs.path[13].addClass('highlighted'), 1000);//dk
-          await sleep(2000);
+          await sleep(1000);
           setTimeout(bfs.path[14].addClass('highlighted'), 1000);//k
-          await sleep(2000);
+          await sleep(1000);
         }
       }
       console.log("iteracion 3");
@@ -322,9 +322,9 @@ async function carAssembleTime(a , t , e , x) {
   //Cuando el final es abajo y todo viene pintado arriba caso de geeks for geeks
   if(first>second){
     setTimeout(bfs.path[11].addClass('dishighlighted'), 1000);//de
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[12].addClass('dishighlighted'), 1000);//e
-    await sleep(2000);
+    await sleep(1000);
     reacomodo=iteracionsiguienteUP[1]+a[1][3]+t[0][3]+x[1];
     /*console.log(iteracionsiguienteUP[1]);//22
     console.log(a[1][3]);//4
@@ -333,79 +333,79 @@ async function carAssembleTime(a , t , e , x) {
     console.log(reacomodo);*/
     if(reacomodo == final){
     setTimeout(bfs.path[13].addClass('highlighted'), 1000);//dk
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[14].addClass('highlighted'), 1000);//k
-    await sleep(2000);
+    await sleep(1000);
     //quitar el caso de cuando las 2 entradas son iguales, en este caso, la parte
     //de abajo se queda coloreada, pero no es parte de la secuencia final
     reacomodo=e[0] + a[0][0];
     reacomodo2= e[1] + a[1][0];
     if(reacomodo == reacomodo2){
     setTimeout(bfs2.path[0].addClass('dishighlighted'), 1000);//g
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[1].addClass('dishighlighted'), 1000);//gh
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[2].addClass('dishighlighted'), 1000);//h
-    await sleep(2000);
+    await sleep(1000);
     }
     }
     else{
     setTimeout(bfs.path[7].addClass('dishighlighted'), 1000);//cd
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[8].addClass('dishighlighted'), 1000);//d
-    await sleep(2000);
+    await sleep(1000);
     reacomodo=iteracionsiguienteUP[0]+t[0][2]+a[1][2]+a[1][3]+x[1];
     if(reacomodo == final){
     setTimeout(bfs.path[9].addClass('highlighted'), 1000);//cj
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[10].addClass('highlighted'), 1000);//j
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[11].addClass('highlighted'), 1000);//jk
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[12].addClass('highlighted'), 1000);//k
-    await sleep(2000);
+    await sleep(1000);
     //quitar el caso de cuando las 2 entradas son iguales, en este caso, la parte
     //de abajo se queda coloreada, pero no es parte de la secuencia final
     reacomodo=e[0] + a[0][0];
     reacomodo2= e[1] + a[1][0];
     if(reacomodo == reacomodo2){
     setTimeout(bfs2.path[0].addClass('dishighlighted'), 1000);//g
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[1].addClass('dishighlighted'), 1000);//gh
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[2].addClass('dishighlighted'), 1000);//h
-    await sleep(2000);
+    await sleep(1000);
     }
     }
     else{
       setTimeout(bfs.path[3].addClass('dishighlighted'), 1000);//bc
-      await sleep(2000);
+      await sleep(1000);
       setTimeout(bfs.path[4].addClass('dishighlighted'), 1000);//c
-      await sleep(2000);
+      await sleep(1000);
     }
     }
   }
   //Cuando el final es arriba y todo viene iluminado abajo
   if(second>first){
     setTimeout(bfs2.path[11].addClass('dishighlighted'), 1000);//jk
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[12].addClass('dishighlighted'), 1000);//k
-    await sleep(2000);
+    await sleep(1000);
   }
 }
 
   //parte final
   if(first==final){
     setTimeout(bfs.path[15].addClass('highlighted'), 1000);//ef
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs.path[16].addClass('highlighted'), 1000);//f
-    await sleep(2000);
+    await sleep(1000);
   }
   else{
     setTimeout(bfs2.path[15].addClass('highlighted'), 1000);//kl
-    await sleep(2000);
+    await sleep(1000);
     setTimeout(bfs2.path[16].addClass('highlighted'), 1000);//l
-    await sleep(2000);
+    await sleep(1000);
   }
   //return final;
   document.getElementById("resultado").innerHTML = "El Tiempo total del ensamblado es: "+final;
