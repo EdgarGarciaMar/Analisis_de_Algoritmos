e4c872e3f0efa189690187a3c8521c0fe2c68ee7
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-12-09 15:17:19 -0600

    update, estilo fuerza

--- Proyecto/js/app.js
+++ Proyecto/js/app.js
@@ -549,25 +549,25 @@ var obtenerdatos=function () {
     //***************Fin de Recepcion de datos del Código de DP***************************** 
 //*************** Recepcion de datos del Código de FB***************************** 
     var obtenerdatosFB=function () {
-      var entradal1 = parseInt(document.getElementById("entradaL1").value);
-      var nodo1l1 = parseInt(document.getElementById("nodo1-1").value);
-      var nodo2l1 = parseInt(document.getElementById("nodo1-2").value);
-      var nodo3l1 = parseInt(document.getElementById("nodo1-3").value);
-      var nodo4l1 = parseInt(document.getElementById("nodo1-4").value);
-      var cambio1l1 = parseInt(document.getElementById("cambiol1-l2-1").value);
-      var cambio2l1 = parseInt(document.getElementById("cambiol1-l2-2").value);
-      var cambio3l1 = parseInt(document.getElementById("cambiol1-l2-3").value);
-      var salidal1 = parseInt(document.getElementById("salidaL1").value);
+       entradal1 = parseInt(document.getElementById("entradaL1").value);
+       nodo1l1 = parseInt(document.getElementById("nodo1-1").value);
+       nodo2l1 = parseInt(document.getElementById("nodo1-2").value);
+       nodo3l1 = parseInt(document.getElementById("nodo1-3").value);
+       nodo4l1 = parseInt(document.getElementById("nodo1-4").value);
+       cambio1l1 = parseInt(document.getElementById("cambiol1-l2-1").value);
+       cambio2l1 = parseInt(document.getElementById("cambiol1-l2-2").value);
+       cambio3l1 = parseInt(document.getElementById("cambiol1-l2-3").value);
+       salidal1 = parseInt(document.getElementById("salidaL1").value);
   
-      var entradal2 = parseInt(document.getElementById("entradaL2").value);
-      var nodo1l2 = parseInt(document.getElementById("nodo2-1").value);
-      var nodo2l2 = parseInt(document.getElementById("nodo2-2").value);
-      var nodo3l2 = parseInt(document.getElementById("nodo2-3").value);
-      var nodo4l2 = parseInt(document.getElementById("nodo2-4").value);
-      var cambio1l2 = parseInt(document.getElementById("cambiol2-l1-1").value);
-      var cambio2l2 = parseInt(document.getElementById("cambiol2-l1-2").value);
-      var cambio3l2 = parseInt(document.getElementById("cambiol2-l1-3").value);
-      var salidal2 = parseInt(document.getElementById("salidaL2").value);
+       entradal2 = parseInt(document.getElementById("entradaL2").value);
+       nodo1l2 = parseInt(document.getElementById("nodo2-1").value);
+       nodo2l2 = parseInt(document.getElementById("nodo2-2").value);
+       nodo3l2 = parseInt(document.getElementById("nodo2-3").value);
+       nodo4l2 = parseInt(document.getElementById("nodo2-4").value);
+       cambio1l2 = parseInt(document.getElementById("cambiol2-l1-1").value);
+       cambio2l2 = parseInt(document.getElementById("cambiol2-l1-2").value);
+       cambio3l2 = parseInt(document.getElementById("cambiol2-l1-3").value);
+       salidal2 = parseInt(document.getElementById("salidaL2").value);
   
       e=new Array();
       e.push(entradal1);
